4e67ccbb0600046f1e396c4ce17338b151917a18
temii-mp3 <ogunleyeamiel@gmail.com>
2025-06-25 19:13:51 -0500

    Removed .vscode from repo

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CardComponent } from '../components/card/card.component';
 import { Card, cardType, cardColor } from '../models/card.model';
+import { GameService } from '../services/game.service';
 @Component({
   selector: 'app-game',
   imports: [CardComponent],
+  providers: [GameService],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  gameService = Inject(GameService)
   colors: cardColor[] = ['red', 'blue', 'green', 'yellow', 'wild'];
   types: cardType[] = []
   playerCard: Card[] = []
   compCard: Card[] = []
   discardTop: Card[] = []
+
+  ngOnInit(): void {
+    this.gameService
+  }
 
 
   getRandomColor(): cardColor{
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -7,5 +7,44 @@ export class GameService {
 
   constructor() { }
 
+
+
+
+
+initializeGame(){
+  if(this.playerCard || this.compCard){
+    this.playerCard = []
+    this.compCard = []
+  }
+  for (let i = 0; i < 7; i++) {
+      const newCard: Card = {
+        id: `card-${i}`,
+        color: this.getRandomColor(),
+        type: this.getRandomType(),
+        value: Math.floor(Math.random() * 10)
+      };
+      this.playerCard.push(newCard);
+    }
+
+  for (let i = 7; i < 14; i++) {
+      const newCard: Card = {
+        id: `card-${i}`,
+        color: this.getRandomColor(),
+        type: this.getRandomType(),
+        value: Math.floor(Math.random() * 10)
+      };
+      this.compCard.push(newCard);
+    }
+
+
+
+    const initialCard: Card = {
+    id: 'initial-card',
+    color: this.getRandomColor(),
+    type: this.getRandomType(),
+    value: Math.floor(Math.random() * 10)
+  }
+  this.discardTop.push(initialCard)
+  }
   
 }
